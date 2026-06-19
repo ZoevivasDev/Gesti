52d36f366ae7d8413334f72853ebb3241991034b
zoevivas <zoe-mari151@hotmail.com>
2026-06-18 22:39:24 -0300

    calendario

--- src/app/modules/feature/calendar/calendar.component.ts
+++ src/app/modules/feature/calendar/calendar.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-//importamos el calendario
+//importi  el calendario
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -9,7 +9,7 @@ import dayGridPlugin from '@fullcalendar/daygrid';
   selector: 'app-calendar',
   imports: [
     CommonModule, 
-    FullCalendarModule //agregamos en los imports del componente
+    FullCalendarModule //agregamos en los imports
   ], 
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
@@ -18,12 +18,16 @@ export class CalendarComponent {
   
   //Configuro las opciones del calendario
   calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',//Vista mensual
+    plugins: [dayGridPlugin],//Agrego el plugin para la vista de cuadrícula diaria
     locale: 'es', // Para que los días y meses salgan en español
+    
+    height: '100%', 
+    expandRows: true,
+    
     events: [
-      { title: 'Turno Control', date: '2026-06-15' },
+      { title: 'evento 1', date: '2026-06-15' },
       { title: 'Turno Limpieza', date: '2026-06-18' }
-    ]
+    ]//Aquí puedes agregar más eventos según tus necesidades
   };
 }
